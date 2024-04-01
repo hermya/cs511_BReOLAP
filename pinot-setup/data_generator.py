ae73e18ffdad5884ba01f014368b27570626d4d6
Hemant Pardeshi <hmnt.pardeshi@gmail.com>
2024-04-01 15:01:07 -0500

    minor changes in pinot file

--- pinot-setup/data_generator.py
+++ pinot-setup/data_generator.py
@@ -2,11 +2,12 @@ import datetime
 import uuid
 import random
 import json
-
-while True:
+i = 0
+while i < 1000 :
     ts = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
     id = str(uuid.uuid4())
     count = random.randint(0, 1000)
     print(
         json.dumps({"ts": ts, "uuid": id, "count": count})
     )
+    i+=1
