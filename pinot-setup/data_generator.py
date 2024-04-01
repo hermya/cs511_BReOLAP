571b6ba553421b26e4bc9cdad8f6269e6edaf8ed
Mihir Gune <mihirgune@gmail.com>
2024-04-01 00:14:36 -0500

    Commiting initial pinot setup

---
+++ pinot-setup/data_generator.py
@@ -0,0 +1,12 @@
+import datetime
+import uuid
+import random
+import json
+
+while True:
+    ts = datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+    id = str(uuid.uuid4())
+    count = random.randint(0, 1000)
+    print(
+        json.dumps({"ts": ts, "uuid": id, "count": count})
+    )
