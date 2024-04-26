701b686d51de70faa835c4f42fcc33a60a9c0d1f
Mihir Gune <mihirgune@gmail.com>
2024-04-25 21:37:00 -0500

    Adding table and schema creation jsons for Pinot and removing unnecessary previous code

--- data_generation/schema/create_table_transactions.sql
+++ data_generation/schema/create_table_transactions.sql
@@ -2,13 +2,11 @@ CREATE TABLE Transactions (
     transaction_id INT AUTO_INCREMENT PRIMARY KEY,
     transaction_uuid UUID DEFAULT (uuid_generate_v4()),
     transaction_date DATE NOT NULL,
-    amount DECIMAL(15, 2) NOT NULL,
+    transaction_amount DECIMAL(15, 2) NOT NULL,
     transaction_type ENUM('Payment', 'Withdrawal', 'InterestPayment', 'LoanRepayment', 'Other') NOT NULL,
-    due_date DATE NOT NULL,
-    category ENUM('Operational', 'Financial', 'ClientWithdrawal', 'Contingent', 'Regulatory') NOT NULL,
-    confirmed BOOLEAN DEFAULT FALSE,
+    transaction_due_date DATE NOT NULL,
+    transaction_category ENUM('Operational', 'Financial', 'ClientWithdrawal', 'Contingent', 'Regulatory') NOT NULL,
+    transaction_confirmed BOOLEAN DEFAULT FALSE,
     asset_linked UUID,
-    counterparty_uuid UUID,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
