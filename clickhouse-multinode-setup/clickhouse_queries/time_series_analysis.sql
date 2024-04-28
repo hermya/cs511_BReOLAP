81021752e311b021c2434a9335ba116a7e48747d
Hemant Pardeshi <hmnt.pardeshi@gmail.com>
2024-04-27 20:35:04 -0500

    added DQL queries for clickhouse

---
+++ clickhouse-multinode-setup/clickhouse_queries/time_series_analysis.sql
@@ -0,0 +1,10 @@
+WITH ranked_prices AS (
+        SELECT asset_name, asset_market_value, timestamp,
+            ROW_NUMBER() OVER (PARTITION BY symbol ORDER BY timestamp) AS rn
+        FROM asset_topic
+        WHERE asset_name IN ('AAPL', 'GOOGL')
+    )
+    SELECT a.asset_name, b.asset_name, CORR(a.asset_market_value, b.asset_market_value) AS correlation
+    FROM ranked_prices a
+    JOIN ranked_prices b ON a.rn = b.rn
+    WHERE a.asset_name = 'AAPL' AND b.asset_name = 'GOOGL';
