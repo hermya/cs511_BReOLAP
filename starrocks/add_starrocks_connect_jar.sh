8fee3638ad20937368bd1847b14ae10f55e60159
hermya <herambku@gmail.com>
2024-05-01 13:37:28 -0500

    Fixed starrocks connector jar download and deploy file

--- starrocks/add_starrocks_connect_jar.sh
+++ starrocks/add_starrocks_connect_jar.sh
@@ -1,7 +1,7 @@
 wget https://github.com/StarRocks/starrocks-connector-for-kafka/releases/download/v1.0.3/starrocks-kafka-connector-1.0.3.tar.gz
 mkdir -p assets
-mv starrocks-kafka-connector-1.0.3.tar.gz assets/
+sudo mv starrocks-kafka-connector-1.0.3.tar.gz assets/
 cd assets
-tar -xvzf starrocks-kafka-connector-1.0.3.tar.gz
+sudo tar -xvzf starrocks-kafka-connector-1.0.3.tar.gz
 cd ..
-rm assets/starrocks-kafka-connector-1.0.3.tar.gz
+sudo rm assets/starrocks-kafka-connector-1.0.3.tar.gz
